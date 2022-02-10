2e4a5b3b2214003207d93c0804917293318c7768
Eduardo Dinato <edudinato@gmail.com>
2022-02-10 16:07:12 -0400

    Unnecessary Comment removed

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,4 @@
 ExUnit.start()
 
-# Mox.defmock(HTTPoison.BaseMock, for: HTTPoison.Base)
 Mox.defmock(HTTPoison.BaseMock, for: HTTPoison.Base)
 Mox.defmock(UrlFetcher.Http.ClientMock, for: UrlFetcher.Http.Adapter)
